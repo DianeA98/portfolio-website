ed975ef8a376c61cd55c8f5d5e1675c1731873e7
DianeA98 <dianeadote@gmail.com>
2023-05-11 01:22:43 -0400

    Add typewriter effect to hero h1

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,16 @@ function menuToggle(){
         x.className = 'navtoggle';
     }
 }
+
+var content = 'Hello, my name is Diane Adote and I design/code beautiful things for the web.';
+
+var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
+
+$(ele).hide().appendTo('h1').each(function (i) {
+    $(this).delay(100 * i).css({
+        display: 'inline',
+        opacity: 0
+    }).animate({
+        opacity: 1
+    }, 100);
+});
